c2b6e2ed079aa635f0eaf4b1c49a0b52973d89c4
efbar <17676301+efbar@users.noreply.github.com>
2020-06-18 15:04:35 +0200

    ehi did you forgot to call it in the right way?

--- modules/cluster-raft/output.tf
+++ modules/cluster-raft/output.tf
@@ -1,3 +1,3 @@
 output "vault_endpoint" {
-  value = google_compute_instance.hcpoc_cluster_nodes[0].network_interface.0.access_config.0.nat_ip
+  value = var.cluster_nodes_public_ips[keys(var.cluster_nodes)[0]]
 }
