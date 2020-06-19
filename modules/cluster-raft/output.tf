62121c47760021c752755f315f00ff76fdc629c3
efbar <17676301+efbar@users.noreply.github.com>
2020-06-20 01:11:42 +0200

    eh

--- modules/cluster-raft/output.tf
+++ modules/cluster-raft/output.tf
@@ -1,3 +1,3 @@
-output "vault_endpoint" {
-  value = var.cluster_nodes_public_ips[keys(var.cluster_nodes)[0]]
+output "vault_rendezvous" {
+  value = var.null_resource.vault_cluster_node_not_1_init[0]
 }
