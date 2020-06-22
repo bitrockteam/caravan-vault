354f79c84868472feb7f4a3b71de3edf6e8f8283
efbar <17676301+efbar@users.noreply.github.com>
2020-06-22 09:48:20 +0200

    monday warm-up

--- modules/cluster-raft/output.tf
+++ modules/cluster-raft/output.tf
@@ -1,3 +1,3 @@
 output "cluster_node_1_public_ip" {
-  value = "${data.null_data_source.values.outputs["cluster_node_1_public_ip"]"
+  value = "${data.null_data_source.values.outputs["cluster_node_1_public_ip"]}"
 }
