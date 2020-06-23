917f27b156c068e65b55cb7a7fa87401a94d1e79
gfdsa <mtabolsky@gmail.com>
2020-06-23 13:55:21 +0200

    Make it work on the inited cluster

--- modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
+++ modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 sudo ls -la /etc/vault.d/
-sudo systemctl start vault &&  \
-sleep 10s && \
-systemctl status vault && \
-vault operator init -address=http://127.0.0.1:8200 |  \
-awk '/Root Token/{print $4}' | \
-sudo tee /root/root_token && \
+sudo systemctl start vault && \
+vault status -address=http://127.0.0.1:8200 | grep -q 'Initialized *true' && exit 0 || {
+  sleep 10s && \
+  systemctl status vault && \
+  vault operator init -address=http://127.0.0.1:8200 | \
+  awk '/Root Token/{print $4}' | \
+  sudo tee /root/root_token
+}
 sleep 10s
