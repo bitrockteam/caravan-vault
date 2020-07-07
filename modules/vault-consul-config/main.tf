cc9287450f24613a53f7437e50125c44fb3f2e41
efbar <17676301+efbar@users.noreply.github.com>
2020-07-07 11:25:06 +0200

    add consul node policy stanza

--- modules/vault-consul-config/main.tf
+++ modules/vault-consul-config/main.tf
@@ -18,3 +18,21 @@ resource "vault_consul_secret_backend_role" "agent_node_role" {
     "cluster-node-agent-policy",
   ]
 }
+
+
+provider "consul" {
+  address    = var.consul_endpoint
+}
+
+resource "consul_acl_policy" "cluster_node_agent_policy" {
+  name        = "cluster-node-agent-policy"
+  rules       = file("${path.module}/acls/cluster-node-agent.hcl")
+}
+
+resource "consul_acl_token_policy_attachment" "cluster_node_agent_token" {
+  depends_on = [
+    consul_acl_policy.cluster_node_agent_policy,
+  ]
+  token_id = data.vault_generic_secret.consul_bootstrap_token.data["accessorid"]
+  policy = consul_acl_policy.cluster_node_agent_policy.name
+}
