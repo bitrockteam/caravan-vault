1f230179d1ca1d43d9c16887aa953a38e6d99a08
gfdsa <mtabolsky@gmail.com>
2020-07-10 17:15:11 +0200

    Added .consul pki

--- modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
+++ modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
@@ -11,4 +11,10 @@ vault status -address=http://127.0.0.1:8200 | grep -q 'Initialized *true' && exi
 }
 sleep 10s
 export VAULT_TOKEN=`sudo cat /root/root_token` VAULT_ADDR=http://127.0.0.1:8200 && \
-vault secrets enable -path=secret kv-v2
+vault secrets enable -path=secret kv-v2 && \
+vault secrets enable pki && \
+vault write pki/root/generate/internal common_name=vault.service.consul ttl=87600h && \
+vault write pki/config/urls issuing_certificates="http://vault.service.consul:8200/v1/pki/ca" crl_distribution_points="http://vault.service.consul:8200/v1/pki/crl" && \
+vault write pki/roles/consul \
+    allowed_domains=consul \
+    allow_subdomains=true max_ttl=72h
