85ffdc268a4966dfa65e35cb004a9ed1befb6278
efbar <17676301+efbar@users.noreply.github.com>
2020-07-15 22:47:14 +0200

    add workers services to catalog

---
+++ modules/vault-consul-config/variables.tf
@@ -0,0 +1,10 @@
+variable "region_instance_group" {
+  type    = string
+  default = "grp-mgr-def-wrkr-grp"
+}
+variable "gcp_project_id" {
+  type = string
+}
+variable "region" {
+  type = string
+}
