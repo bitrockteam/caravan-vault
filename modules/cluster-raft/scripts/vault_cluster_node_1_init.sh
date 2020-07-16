f836fe9226120751bd9ce978222c7c1448f5e612
efbar <17676301+efbar@users.noreply.github.com>
2020-07-17 01:27:10 +0200

    add pki handling

--- modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
+++ modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
@@ -13,8 +13,31 @@ sleep 10s
 export VAULT_TOKEN=`sudo cat /root/root_token` VAULT_ADDR=http://127.0.0.1:8200 && \
 vault secrets enable -path=secret kv-v2 && \
 vault secrets enable pki && \
-vault write pki/root/generate/internal common_name=vault.service.consul ttl=87600h && \
-vault write pki/config/urls issuing_certificates="http://vault.service.consul:8200/v1/pki/ca" crl_distribution_points="http://vault.service.consul:8200/v1/pki/crl" && \
-vault write pki/roles/consul \
-    allowed_domains=consul \
-    allow_subdomains=true max_ttl=72h
+vault secrets tune -max-lease-ttl=87600h pki && \
+vault write -field=certificate pki/root/generate/internal common_name="consul" ttl=87600h > CA_cert.crt && \
+vault write pki/config/urls issuing_certificates="http://127.0.0.1:8200/v1/pki/ca" crl_distribution_points="http://127.0.0.1:8200/v1/pki/crl" && \
+vault secrets enable -path=pki_int pki && \
+vault secrets tune -max-lease-ttl=43800h pki_int && \
+vault write -format=json pki_int/intermediate/generate/internal common_name="consul Intermediate Authority" | jq -r '.data.csr' | sudo tee /root/pki_intermediate.csr && \
+vault write -format=json pki/root/sign-intermediate csr=@pki_intermediate.csr format=pem_bundle ttl="43800h" | jq -r '.data.certificate' | sudo tee /root/intermediate.cert.pem && \
+vault write pki_int/intermediate/set-signed certificate=@intermediate.cert.pem && \
+vault write pki_int/roles/consul allowed_domains="consul" allow_subdomains=true max_ttl="720h" && \
+vault write pki_int/issue/consul common_name="consul" ttl="24h" | sudo tee /root/certs.json
+##### this in the oven, look at consul tmpl #####
+# {{ with secret "pki_int/issue/consul" "common_name=consul" "ttl=24h" }}
+# {{ .Data.certificate }}
+# {{ .Data.issuing_ca }}
+# {{ .Data.private_key }}
+# {{ end }}
+
+
+
+# vault write pki/root/generate/internal common_name=vault.service.consul ttl=87600h && \
+# vault write pki/config/urls issuing_certificates="http://vault.service.consul:8200/v1/pki/ca" crl_distribution_points="http://vault.service.consul:8200/v1/pki/crl" && \
+# vault write pki/roles/consul \
+#     allowed_domains=consul \
+#     allow_subdomains=true max_ttl=72h
+
+
+
+
