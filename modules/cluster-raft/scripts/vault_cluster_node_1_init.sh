65a45a72ad3e12618e03c675ab52644177d1617a
efbar <17676301+efbar@users.noreply.github.com>
2020-07-17 17:46:58 +0200

    clean after pki creation

--- modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
+++ modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
@@ -14,7 +14,7 @@ export VAULT_TOKEN=`sudo cat /root/root_token` VAULT_ADDR=http://127.0.0.1:8200 
 vault secrets enable -path=secret kv-v2 && \
 vault secrets enable pki && \
 vault secrets tune -max-lease-ttl=87600h pki && \
-vault write -field=certificate pki/root/generate/internal common_name="consul" ttl=87600h > CA_cert.crt && \
+vault write -field=certificate pki/root/generate/internal common_name="consul" ttl=87600h && \
 vault write pki/config/urls issuing_certificates="http://127.0.0.1:8200/v1/pki/ca" crl_distribution_points="http://127.0.0.1:8200/v1/pki/crl" && \
 vault secrets enable -path=pki_int pki && \
 vault secrets tune -max-lease-ttl=43800h pki_int && \
@@ -22,7 +22,8 @@ vault write -format=json pki_int/intermediate/generate/internal common_name="con
 vault write -format=json pki/root/sign-intermediate csr=@pki_intermediate.csr format=pem_bundle ttl="43800h" | jq -r '.data.certificate' | tee intermediate.cert.pem && \
 vault write pki_int/intermediate/set-signed certificate=@intermediate.cert.pem && \
 vault write pki_int/roles/consul allowed_domains="consul" allow_subdomains=true allow_any_name=true max_ttl="720h" && \
-vault write pki_int/issue/consul common_name="consul" ttl="24h" | tee certs.json
+vault write -format=json pki_int/issue/consul common_name="consul" ttl="24h" | tee certs.json && \
+rm pki_intermediate.csr intermediate.cert.pem
 ##### this in the oven, look at consul tmpl #####
 # {{ with secret "pki_int/issue/consul" "common_name=consul" "ttl=24h" }}
 # {{ .Data.certificate }}
