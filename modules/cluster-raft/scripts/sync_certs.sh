0da0385be017b667829781c96684d5b9b777b09c
efbar <17676301+efbar@users.noreply.github.com>
2020-07-20 14:13:49 +0200

    add certs sync scripts

---
+++ modules/cluster-raft/scripts/sync_certs.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+set -e
+
+export VAULT_TOKEN=`sudo cat /root/root_token` VAULT_ADDR=http://127.0.0.1:8200 && \
+vault write -format=json pki_int/issue/consul common_name="consul" ttl="168h" | tee certs.json && \
+cat jq -r .data.issuing_ca | sudo tee /etc/consul.d/ca && \
+cat jq -r .data.certificate | sudo tee /etc/consul.d/cert && \
+cat jq -r .data.private_key | sudo tee /etc/consul.d/keyfile && \
+sudo chown vault:consul /etc/consul.d/ca /etc/consul.d/cert /etc/consul.d/keyfile
+rm -rf certs.json
