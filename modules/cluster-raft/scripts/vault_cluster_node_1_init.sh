eed9744cd4e06b8341b4a34d2afac07abe9958f2
efbar <17676301+efbar@users.noreply.github.com>
2020-07-24 00:53:20 +0200

    creepy

--- modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
+++ modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
@@ -14,14 +14,15 @@ export VAULT_TOKEN=`sudo cat /root/root_token` VAULT_ADDR=http://127.0.0.1:8200 
 vault secrets enable -path=secret kv-v2 && \
 vault secrets enable pki && \
 vault secrets tune -max-lease-ttl=87600h pki && \
-vault write -field=certificate pki/root/generate/internal common_name="service.consul" ttl=87600h && \
+vault write -field=certificate pki/root/generate/internal common_name="consul" ttl=87600h && \
 vault write pki/config/urls issuing_certificates="http://127.0.0.1:8200/v1/pki/ca" crl_distribution_points="http://127.0.0.1:8200/v1/pki/crl" && \
 vault secrets enable -path=pki_int pki && \
 vault secrets tune -max-lease-ttl=43800h pki_int && \
-vault write -format=json pki_int/intermediate/generate/internal common_name="Consul Intermediate Authority" alt_names="localhost" ip_sans="127.0.0.1" | jq -r '.data.csr' | tee pki_intermediate.csr && \
+vault write -format=json pki_int/intermediate/generate/internal common_name="Consul Intermediate Authority" alt_names="localhost,127.0.0.1" ip_sans="localhost,127.0.0.1" | jq -r '.data.csr' | tee pki_intermediate.csr && \
 vault write -format=json pki/root/sign-intermediate csr=@pki_intermediate.csr format=pem_bundle ttl="43800h" | jq -r '.data.certificate' | tee intermediate.cert.pem && \
 vault write pki_int/intermediate/set-signed certificate=@intermediate.cert.pem && \
-vault write pki_int/roles/consul allowed_domains="service.consul" allow_subdomains=true max_ttl="720h" && \
+vault write pki_int/roles/consul allowed_domains="consul" allow_subdomains=true max_ttl="720h" && \
+vault write pki_int/roles/nomad allowed_domains="nomad" allow_subdomains=true max_ttl="720h" && \
 rm pki_intermediate.csr intermediate.cert.pem
 ##### this in the oven, look at consul tmpl #####
 # {{ with secret "pki_int/issue/consul" "common_name=consul" "ttl=24h" }}
