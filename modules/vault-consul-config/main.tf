fa9262ced0b5544b8d62f47c15cd03563bbd0008
efbar <17676301+efbar@users.noreply.github.com>
2020-07-24 09:26:02 +0200

    bring back those useless s#$*!

--- modules/vault-consul-config/main.tf
+++ modules/vault-consul-config/main.tf
@@ -55,3 +55,57 @@ resource "vault_generic_secret" "ui_token" {
 }
 EOT
 }
+
+resource "consul_service" "consul_servers" {
+  count = 3
+  name    = "consul-node0${count.index + 1}"
+  node    = "clustnode0${count.index + 1}"
+  port    = 8300
+  tags    = ["consul", "server"]
+  check {
+    check_id                          = "service:consul-node0${count.index + 1}"
+    name                              = "Consul health check"
+    status                            = "passing"
+    tcp                               = "127.0.0.1:8300"
+    tls_skip_verify                   = false
+    interval                          = "10s"
+    timeout                           = "5s"
+  }
+}
+
+
+resource "consul_service" "dnsmasq_clusternode" {
+  count = 3
+  name    = "dnsmasq_clusternode0${count.index + 1}"
+  node    = "clustnode0${count.index + 1}"
+  port    = 53
+  tags    = ["dnsmasq"]
+  check {
+    # nslookup soa consul
+    check_id                          = "service:dnsmasq0${count.index + 1}"
+    name                              = "DNSmasq health check"
+    status                            = "passing"
+    tcp                               = "127.0.0.1:53"
+    tls_skip_verify                   = false
+    interval                          = "10s"
+    timeout                           = "5s"
+    deregister_critical_service_after = "30s"
+  }
+}
+
+resource "consul_service" "vault" {
+  count = 3
+  name    = "vault0${count.index + 1}"
+  node    = "clustnode0${count.index + 1}"
+  port    = 8200
+  tags    = ["vault"]
+  check {
+    check_id                          = "service:vault0${count.index + 1}"
+    name                              = "Vault health check"
+    status                            = "passing"
+    tcp                               = "127.0.0.1:8200"
+    tls_skip_verify                   = false
+    interval                          = "10s"
+    timeout                           = "5s"
+  }
+}
