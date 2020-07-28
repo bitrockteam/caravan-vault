6b7a8b6f58ba65158616c1014af7909162b90bcc
efbar <17676301+efbar@users.noreply.github.com>
2020-07-28 12:03:59 +0200

    remove second jaeger

--- modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
+++ modules/cluster-raft/scripts/vault_cluster_node_1_init.sh
@@ -13,10 +13,10 @@ sleep 10s
 export VAULT_TOKEN=`sudo cat /root/root_token` VAULT_ADDR=http://127.0.0.1:8200 && \
 vault secrets enable -path=secret kv-v2 && \
 #### connect pki
-vault secrets enable -path=connect_pki pki && \
-vault secrets tune -max-lease-ttl=87600h connect_pki && \
-vault write -field=certificate connect_pki/root/generate/internal common_name="connect" ttl=87600h && \
-vault write connect_pki/config/urls issuing_certificates="http://127.0.0.1:8200/v1/connect_pki/ca" crl_distribution_points="http://127.0.0.1:8200/v1/connect_pki/crl" && \
+# vault secrets enable -path=connect_pki pki && \
+# vault secrets tune -max-lease-ttl=87600h connect_pki && \
+# vault write -field=certificate connect_pki/root/generate/internal common_name="connect" ttl=87600h && \
+# vault write connect_pki/config/urls issuing_certificates="http://127.0.0.1:8200/v1/connect_pki/ca" crl_distribution_points="http://127.0.0.1:8200/v1/connect_pki/crl" && \
 # vault secrets enable -path=pki_int pki && \
 # vault secrets tune -max-lease-ttl=43800h pki_int && \
 # vault write -format=json pki_int/intermediate/generate/internal common_name="connect Intermediate Authority" alt_names="localhost,127.0.0.1" ip_sans="127.0.0.1" | jq -r '.data.csr' | tee pki_intermediate.csr && \
@@ -24,7 +24,7 @@ vault write connect_pki/config/urls issuing_certificates="http://127.0.0.1:8200/
 # vault write pki_int/intermediate/set-signed certificate=@intermediate.cert.pem && \
 # vault write pki_int/roles/connect allowed_domains="connect,127.0.0.1" allow_subdomains=true max_ttl="720h" && \
 # rm pki_intermediate.csr intermediate.cert.pem
-#### consul/nomad pki
+#### consul/connect/nomad pki
 vault secrets enable -path=tls_pki pki && \
 vault secrets tune -max-lease-ttl=87600h tls_pki && \
 vault write -field=certificate tls_pki/root/generate/internal common_name="consul" ttl=87600h && \
