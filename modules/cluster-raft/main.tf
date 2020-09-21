a22777eb068b66813a054cb8588c0a796ea8d85d
efbar <17676301+efbar@users.noreply.github.com>
2020-09-21 18:32:41 +0200

    rename files

--- modules/cluster-raft/main.tf
+++ modules/cluster-raft/main.tf
@@ -105,19 +105,19 @@ resource "null_resource" "copy_root_token" {
     environment = {
       SOURCE_HOST = var.cluster_nodes_public_ips != null ? var.cluster_nodes_public_ips[keys(var.cluster_nodes)[0]] : var.cluster_nodes[keys(var.cluster_nodes)[0]]
     }
-    command = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${path.module}/.ssh-key ${var.ssh_user}@$SOURCE_HOST 'sudo cat /root/.lb-cert.json | jq -r .certificate' > .lb-cert.json"
+    command = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${path.module}/.ssh-key ${var.ssh_user}@$SOURCE_HOST 'sudo cat /root/.lb-cert.json | jq -r .certificate' > .lb-cert.pem"
   }
   provisioner "local-exec" {
     environment = {
       SOURCE_HOST = var.cluster_nodes_public_ips != null ? var.cluster_nodes_public_ips[keys(var.cluster_nodes)[0]] : var.cluster_nodes[keys(var.cluster_nodes)[0]]
     }
-    command = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${path.module}/.ssh-key ${var.ssh_user}@$SOURCE_HOST 'sudo cat /root/.lb-cert.json | jq -r .private_key' > .lb-key.json"
+    command = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${path.module}/.ssh-key ${var.ssh_user}@$SOURCE_HOST 'sudo cat /root/.lb-cert.json | jq -r .private_key' > .lb-key.pem"
   }
   provisioner "local-exec" {
     environment = {
       SOURCE_HOST = var.cluster_nodes_public_ips != null ? var.cluster_nodes_public_ips[keys(var.cluster_nodes)[0]] : var.cluster_nodes[keys(var.cluster_nodes)[0]]
     }
-    command = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${path.module}/.ssh-key ${var.ssh_user}@$SOURCE_HOST 'sudo cat /root/.lb-cert.json | jq -r .issuing_ca' > .lb-ca.json"
+    command = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${path.module}/.ssh-key ${var.ssh_user}@$SOURCE_HOST 'sudo cat /root/.lb-cert.json | jq -r .issuing_ca' > .lb-ca.pem"
   }
   provisioner "local-exec" {
     command = "rm ${path.module}/.ssh-key"
