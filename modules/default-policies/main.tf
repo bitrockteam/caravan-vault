eb3205046f7311e544c1d90cb36a27b1cdfdbb97
efbar <17676301+efbar@users.noreply.github.com>
2020-10-05 11:29:26 +0200

    add nomad cluster policy and role

--- modules/default-policies/main.tf
+++ modules/default-policies/main.tf
@@ -8,3 +8,16 @@ resource "vault_policy" "vault_admin" {
   policy = file("${path.module}/policies/vault-admin-policy.hcl")
 }
 
+resource "vault_policy" "nomad_cluster" {
+  name   = "nomad-server"
+  policy = file("${path.module}/policies/nomad-server-policy.hcl")
+}
+
+resource "vault_token_auth_backend_role" "nomad-cluster" {
+  role_name           = "nomad-cluster"
+  disallowed_policies = ["nomad-server"]
+  orphan              = true
+  period              = "86400"
+  renewable           = true
+  explicit_max_ttl    = "115200"
+}
