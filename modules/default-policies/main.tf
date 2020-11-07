da85148c7b6f3569901352d7139272ea73948305
efbar <17676301+efbar@users.noreply.github.com>
2020-11-07 16:42:32 +0100

    fix policy name\

--- modules/default-policies/main.tf
+++ modules/default-policies/main.tf
@@ -38,7 +38,7 @@ resource "vault_policy" "nomad_anon_restricted" {
 
 resource "vault_token_auth_backend_role" "nomad-cluster-node" {
   role_name                 = "cluster-node"
-  allowed_policies          = [ "nomad-app-devs-volumes-policy", "nomad-ops"]
+  allowed_policies          = [ "nomad-app-devs-volumes", "nomad-ops" ]
   orphan                    = true
   token_period              = "259200"
   renewable                 = true
