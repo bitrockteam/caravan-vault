1acd3b83e9ea79de37f49b479eb23e46cb4a510d
efbar <17676301+efbar@users.noreply.github.com>
2021-02-01 12:14:03 +0100

    remove "Interpolation-only expressions"

--- modules/vault-consul-config/main.tf
+++ modules/vault-consul-config/main.tf
@@ -64,11 +64,11 @@ resource "consul_acl_token" "ui_token" {
 }
 resource "consul_acl_token" "nomad_server_token" {
   description = "nomad server policy token"
-  policies    = ["${consul_acl_policy.nomad_server_policy.name}"]
+  policies    = [consul_acl_policy.nomad_server_policy.name]
 }
 resource "consul_acl_token" "nomad_client_token" {
   description = "nomad client policy token"
-  policies    = ["${consul_acl_policy.nomad_client_policy.name}"]
+  policies    = [consul_acl_policy.nomad_client_policy.name]
 }
 
 data "consul_acl_token_secret_id" "ui_token" {
