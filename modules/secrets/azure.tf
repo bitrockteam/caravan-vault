9d3ea6857b2288e045cc1c9c429c7560af96e13b
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-02-22 15:48:40 +0100

    terraform fmt <3

--- modules/secrets/azure.tf
+++ modules/secrets/azure.tf
@@ -2,19 +2,19 @@ resource "vault_azure_secret_backend" "azure" {
   count = var.azure_csi ? 1 : 0
 
   subscription_id = var.azure_subscription_id
-  tenant_id = var.azure_tenant_id
-  client_id = var.azure_client_id
-  client_secret = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
 }
 
 resource "vault_azure_secret_backend_role" "contributor" {
   count = var.azure_csi ? 1 : 0
 
   backend = vault_azure_secret_backend.azure[0].path
-  role = "contributor"
+  role    = "contributor"
 
   azure_roles {
     role_name = "Contributor"
-    scope =  "/subscriptions/${var.azure_subscription_id}/resourceGroups/${var.azure_resource_group}"
+    scope     = "/subscriptions/${var.azure_subscription_id}/resourceGroups/${var.azure_resource_group}"
   }
 }
