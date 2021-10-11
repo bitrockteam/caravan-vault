53f072fe65afd448c54c5646196854cbad93bf33
efbar <17676301+efbar@users.noreply.github.com>
2021-10-11 18:59:00 +0200

    concat them

--- modules/default-policies/outputs.tf
+++ modules/default-policies/outputs.tf
@@ -1,7 +1,7 @@
 output "control_plane_policies" {
-  value = var.enable_nomad ? [vault_policy.consul_agent.name, vault_policy.nomad_server.*.name] : [vault_policy.consul_agent.name]
+  value = concat(vault_policy.consul_agent.name, vault_policy.nomad_server.*.name)
 }
 
 output "worker_plane_policies" {
-  value = var.enable_nomad ? [vault_policy.consul_agent.name, vault_policy.nomad_app_devs_volumes.*.name] : [vault_policy.consul_agent.name]
+  value = concat(vault_policy.consul_agent.name, vault_policy.nomad_app_devs_volumes.*.name)
 }
