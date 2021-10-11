4f0cc23c8bf101ac7c9f55a90030be45966d4a69
efbar <17676301+efbar@users.noreply.github.com>
2021-10-11 19:05:31 +0200

    fix list

--- modules/default-policies/outputs.tf
+++ modules/default-policies/outputs.tf
@@ -1,7 +1,7 @@
 output "control_plane_policies" {
-  value = concat(tolist(vault_policy.consul_agent.name, vault_policy.nomad_server.*.name))
+  value = concat(tolist(vault_policy.consul_agent.name), vault_policy.nomad_server.*.name)
 }
 
 output "worker_plane_policies" {
-  value = concat(tolist(vault_policy.consul_agent.name, vault_policy.nomad_app_devs_volumes.*.name))
+  value = concat(tolist(vault_policy.consul_agent.name), vault_policy.nomad_app_devs_volumes.*.name)
 }
