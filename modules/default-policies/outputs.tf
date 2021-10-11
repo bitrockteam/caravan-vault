df1712bfb16c9dab87a9695ffc6e3975cb609c91
efbar <17676301+efbar@users.noreply.github.com>
2021-10-11 19:13:57 +0200

    compacting

--- modules/default-policies/outputs.tf
+++ modules/default-policies/outputs.tf
@@ -1,7 +1,7 @@
 output "control_plane_policies" {
-  value = concat([vault_policy.consul_agent.name], [vault_policy.nomad_server.*.name])
+  value = compact([vault_policy.consul_agent.name, vault_policy.nomad_server.*.name])
 }
 
 output "worker_plane_policies" {
-  value = concat([vault_policy.consul_agent.name], [vault_policy.nomad_app_devs_volumes.*.name])
+  value = compact([vault_policy.consul_agent.name, vault_policy.nomad_app_devs_volumes.*.name])
 }
