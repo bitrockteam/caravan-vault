64a8112790b89605592bdd59c2fe1f51b5e571bb
efbar <17676301+efbar@users.noreply.github.com>
2021-10-11 18:34:14 +0200

    better output

--- modules/default-policies/outputs.tf
+++ modules/default-policies/outputs.tf
@@ -1,7 +1,7 @@
 output "control_plane_policies" {
-  value = [vault_policy.consul_agent.name, vault_policy.nomad_server.*.name]
+  value = var.enable_nomad ? [vault_policy.consul_agent.name, vault_policy.nomad_server.*.name] : [vault_policy.consul_agent.name]
 }
 
 output "worker_plane_policies" {
-  value = [vault_policy.consul_agent.name, vault_policy.nomad_app_devs_volumes.*.name]
+  value = var.enable_nomad ? [vault_policy.consul_agent.name, vault_policy.nomad_app_devs_volumes.*.name] : [vault_policy.consul_agent.name]
 }
